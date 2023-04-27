07a79460ba79026144c84c0990e60a9a67b04c95
Brendan O'Connor <20098888@wit.ie>
2023-04-27 16:18:00 +0100

    Add some functions to FranchiseAPI, add Utilities.kt

---
+++ src/main/kotlin/utils/Utilities.kt
@@ -0,0 +1,21 @@
+package utils
+
+import models.Game
+import models.Franchise
+
+object Utilities {
+
+    // NOTE: JvmStatic annotation means that the methods are static i.e. we can call them over the class
+    //      name; we don't have to create an object of Utilities to use them.
+
+    @JvmStatic
+    fun formatListString(notesToFormat: List<Franchise>): String =
+        notesToFormat
+            .joinToString(separator = "\n") { franchise ->  "$franchise" }
+
+    @JvmStatic
+    fun formatSetString(itemsToFormat: Set<Game>): String =
+        itemsToFormat
+            .joinToString(separator = "\n") { game ->  "\t$game" }
+
+}
