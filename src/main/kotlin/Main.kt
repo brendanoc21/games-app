ba07b75824031e52a03d09c08ff10050bec155fe
Brendan O'Connor <20098888@wit.ie>
2023-04-27 15:40:29 +0100

    Update menu layout

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ import utils.ScannerInput.readNextInt
 import utils.ScannerInput.readNextLine
 import java.io.File
 import java.lang.System.exit
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     runMenu()
@@ -14,21 +15,31 @@ fun main(args: Array<String>) {
 fun mainMenu(): Int {
     return ScannerInput.readNextInt(
         """ 
-         > ----------------------------------
-         > /        NOTES APP PROJECT       /
-         > ----------------------------------
-         > / NOTE MENU                      /
-         > /   1) Add a franchise           /
-         > /   2) List franchises           /
-         > /   3) Modify franchises         /
-         > /   4) Search franchises         /
-         > /   5) Count franchises          /
-         > /                                /
-         > /   6) Save franchises           /
-         > /   7) Load franchises           /
-         > ----------------------------------
-         > /   0) Exit                      /
-         > ----------------------------------
+         > --------------------------------------
+         > /        FRANCHISE APP PROJECT       /
+         > --------------------------------------
+         > / FRANCHISE MENU                     /
+         > /   1) Add a franchise               /
+         > /   2) Update franchises             /
+         > /   3) Delete franchises             /
+         > /   4) List franchises               /
+         > --------------------------------------
+         > / GAME MENU                          /
+         > /   5) Add a game to a franchise     /
+         > /   6) Update a game from a franchise/
+         > /   7) Delete a game from a franchise/
+         > /   8) List all games                /
+         > --------------------------------------
+         > / REPORT MENU                        /
+         > /   9) Search all franchises         /
+         > /  10) Search all games              /
+         > --------------------------------------
+         > / DATA MENU                          /
+         > /  11) Save data                     /
+         > /  12) Load data                     /
+         > --------------------------------------
+         > /   0) Exit                          /
+         > --------------------------------------
          > ==>> """.trimMargin(">")
     )
 }
@@ -38,14 +49,27 @@ fun runMenu() {
         val option = mainMenu()
         when (option) {
             //1 -> addFranchise()
-            //2 -> listFranchises()
-            //3 -> modify()
-            //4 -> search()
-            //5 -> count()
-            //6 -> save()
-            //7 -> load()
-            //0 -> exitApp()
+            //2 -> updateFranchise()
+            //3 -> deleteFranchise()
+            //4 -> listFranchises()
+
+            //5 -> addGame()
+            //6 -> updateGame()
+            //7 -> deleteGame()
+            //8 -> listGames()
+
+            //9 -> searchFranchise()
+            //10 -> searchGames()
+
+            //11 -> save()
+            //12 -> load()
+            0 -> exit()
             else -> println("Invalid option entered: $option")
         }
     } while (true)
+}
+
+fun exit() {
+    println("Exiting app")
+    exitProcess(0)
 }
