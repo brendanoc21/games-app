42238e77b6ef5248c72c2b603676031964feec0d
Brendan O'Connor <20098888@wit.ie>
2023-04-27 16:48:24 +0100

    Add game functionality to franchise

--- src/main/kotlin/models/Franchise.kt
+++ src/main/kotlin/models/Franchise.kt
@@ -1,5 +1,7 @@
 package models
 
+import utils.Utilities
+
 data class Franchise(
     var franId: Int = 0,
     var franName: String,
@@ -8,5 +10,39 @@ data class Franchise(
     var franGenre: String,
     var franActivity: Boolean = false,
     var games : MutableSet<Game> = mutableSetOf()){
+
+    private var lastGameId = 0
+    private fun getGameId() = lastGameId++
+
+    fun addGame(game: Game) : Boolean {
+        game.gameId = getGameId()
+        return games.add(game)
+    }
+
+    fun numberOfGames() = games.size
+
+    fun findOne(id: Int): Game?{
+        return games.find{ game -> game.gameId == id }
+    }
+
+    fun delete(id: Int): Boolean {
+        return games.removeIf { game -> game.gameId == id}
+    }
+
+    fun update(id: Int, newGame : Game): Boolean {
+        val foundGame = findOne(id)
+
+        if (foundGame != null){
+            foundGame.gameName = newGame.gameName
+            foundGame.gamePrice = newGame.gamePrice
+            foundGame.gameProduced = newGame.gameProduced
+            return true
+        }
+        return false
+    }
+
+    fun listGames() =
+        if (games.isEmpty())  "\tNo Games Added"
+        else  Utilities.formatSetString(games)
 
 }
