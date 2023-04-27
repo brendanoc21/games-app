81a6537afffc5dc482db3b29e21b6afc2fc27c9d
Brendan O'Connor <20098888@wit.ie>
2023-04-27 14:16:33 +0100

    Add basic menu to Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,51 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import controllers.FranchiseAPI
+import models.Franchise
+import models.Game
+import utils.ScannerInput
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
+import java.io.File
+import java.lang.System.exit
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    runMenu()
+}
+
+fun mainMenu(): Int {
+    return ScannerInput.readNextInt(
+        """ 
+         > ----------------------------------
+         > /        NOTES APP PROJECT       /
+         > ----------------------------------
+         > / NOTE MENU                      /
+         > /   1) Add a franchise           /
+         > /   2) List franchises           /
+         > /   3) Modify franchises         /
+         > /   4) Search franchises         /
+         > /   5) Count franchises          /
+         > /                                /
+         > /   6) Save franchises           /
+         > /   7) Load franchises           /
+         > ----------------------------------
+         > /   0) Exit                      /
+         > ----------------------------------
+         > ==>> """.trimMargin(">")
+    )
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            //1 -> addFranchise()
+            //2 -> listFranchises()
+            //3 -> modify()
+            //4 -> search()
+            //5 -> count()
+            //6 -> save()
+            //7 -> load()
+            //0 -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    } while (true)
 }
